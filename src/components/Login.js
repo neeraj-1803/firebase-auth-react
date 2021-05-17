6687d2bd34ea9da34e417c7f263a22abc1ffc9ac
Neeraj Dwivedi <neeraj.dwivedi@kony.com>
2021-05-17 23:35:34 +0530

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import React, {useRef, useState} from 'react'
+import {Form, Button, Card, Alert} from 'react-bootstrap'
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from "../context/AuthContext";
+
+function Login() {
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const {login} = useAuth();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+    const history = useHistory();
+
+    async function handleSubmit(e){
+        e.preventDefault();
+        try{
+            setError('');
+            setLoading(true);
+            await login(emailRef.current.value, passwordRef.current.value);
+            history.push("/");
+        }catch{
+            setError('Failed to sing in');
+        }
+        setLoading(false);
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className="text-center mb-4">Log In</h2>
+                    {error && <Alert variant="danger">{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id="email" className="mb-2">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" ref={emailRef} required />
+                        </Form.Group>
+                        <Form.Group id="password" className="mb-2">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" ref={passwordRef} required />
+                        </Form.Group>
+                        <Button disabled={loading} type="submit" className="w-100 mt-4">Log In</Button>
+                    </Form>
+                    <div className="w-100 text-center mt-3">
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </div>
+                </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+                Need an account? <Link to="/signup">Sign Up</Link>
+            </div>
+        </>
+    )
+}
+
+export default Login
